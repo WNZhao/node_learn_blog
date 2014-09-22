c1e4a6d8a0d640c3fd47ac2e82a8be7134373f77
aliyun_zhaown <activationzwn@126.com>
2014-09-22 14:20:07 +0800

    add session support

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var routes = require('./routes');
 //var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var MongoStore = require('connect-mongo')(express);
+var settings = require("./settings");
 var app = express();
 
 // all environments
@@ -19,6 +21,22 @@ app.use(express.logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use(express.cookieParser());
+
+app.use(express.session({
+     secret:settings.cookieSercet,
+     key:settings.db,
+     cookie:{maxAge:1000*60*60*24*30},
+     store:new MongoStore({
+        db:settings.db,
+	    host:settings.host,
+	    port:settings.port
+     })
+
+}));
+
+
+
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
