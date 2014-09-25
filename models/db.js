5e161fb80ea3d2b9bdc7e1c324a7296e591718ec
aliyun_zhaown <activationzwn@126.com>
2014-09-25 16:53:22 +0800

    simple login logout post article inf display article

--- models/db.js
+++ models/db.js
@@ -1,5 +1,5 @@
 var settings = require('../settings');
 Db = require('mongodb').Db,
 Connection = require('mongodb').Connection,
-Server = requrie('mongodb').Server;
+Server = require('mongodb').Server;
 module.exports = new Db(settings.db,new Server(settings.host,20000),{safe:true});
--- routes/index.js
+++ routes/index.js
@@ -7,15 +7,50 @@
    /logout:登出
  */
 
-var crypto = require('crypto'),User = require('../models/user.js');
-
+var crypto = require('crypto'),User = require('../models/user.js'),Post = require('../models/post.js');
+//-----------------路由中间件----------------
+  //类似过滤器
+  function checkLogin(req,res,next){
+     if(!req.session.user){
+        req.flash('error','未登录！');
+        res.redirect('/login');
+     }
+     next();
+  }
+  function checkNotLogin(req,res,next){
+      if(req.session.user){
+         req.flash('error','已登录！');
+         res.redirect('back');
+      }
+      next();
+  }
+//-------------------------------------------
 module.exports = function(app){
   app.get('/',function(req,res){
-        res.render('index', { title: 'little bird-主页' });
+         Post.get(null,function(err,posts){
+             if(err){
+               posts = [];
+             }
+         // console.log(posts);   
+        res.render('index', { 
+                               title: 'little bird-主页'
+                              ,user:req.session.user
+                              ,posts:posts
+                              ,success:req.flash('success').toString() 
+                              ,error:req.flash('error').toString() 
+         });
+                            });
   });
+  app.get('/reg',checkNotLogin);
   app.get('/reg',function(req,res){
-        res.render('reg',{title:'注册'});
+        res.render('reg',{
+                           title:'注册'
+                           ,user:req.session.user
+                           ,success:req.flash('success').toString()
+                           ,error:req.flash('error').toString()
+                          });
   });
+  app.post('/reg',checkNotLogin);
   app.post('/reg',function(req,res){
      var name = req.body.name,
          password = req.body.password,
@@ -29,7 +64,7 @@ module.exports = function(app){
          password = md5.update(req.body.password).digest('hex');
      var newUser = new User({
             name:name,
-            password,password,
+            password:password,
             email:req.body.email
          });
      //check use exists or not
@@ -51,23 +86,62 @@ module.exports = function(app){
      });
         
   });
+  app.get('/login',checkNotLogin);
   app.get('/login',function(req,res){
-        res.render('login',{'title':'登录'});
+        res.render('login',{
+                              'title':'登录'
+                              ,user:req.session.user
+                              ,success:req.flash('success').toString()
+                              ,error:req.flash('error').toString()
+                            });
   });
+  app.post('/login',checkNotLogin);
   app.post('/login',function(req,res){
+       var md5 = crypto.createHash('md5');
+       var password = md5.update(req.body.password).digest('hex');
+       User.get(req.body.name,function(err,user){
+          if(!user){
+            req.flash('error','用户不存在!');
+            return res.redirect('/login');
+          }
+          if(user.password!=password){
+              req.flash('error','密码错误！');
+              return res.redirect('login'); 
+          }
+          req.session.user = user;
+          req.flash('success','登录成功！');
+          res.redirect('/');
+       });
   
   });
-  
+  app.get('/post',checkLogin);
   app.get('/post',function(req,res){
-      res.render('post',{title:'发表'});
+      res.render('post', { 
+                               title: 'little bird-主页'
+                              ,user:req.session.user
+                              ,success:req.flash('success').toString() 
+                              ,error:req.flash('error').toString() 
+         });
   });
-  
-  app.post('/post',function(req,res){
 
+  app.post('/post',checkLogin);  
+  app.post('/post',function(req,res){
+      var currentUser = req.session.user,
+       post = new Post(currentUser.name,req.body.title,req.body.post);
+       post.save(function(err){
+           if(err){
+              req.flash("error",err);
+              return res.redirect('/');
+           }
+           req.flash('success','发布成功！');
+           res.redirect("/");
+       });
   });
 
   app.get('/logout',function(req,res){
-
+      req.session.user = null;
+      req.flash('success','登出成功！');
+      res.redirect('/');
   });
   
 };
