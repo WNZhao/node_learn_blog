57c8d8cde881c8e68c457561313ffdf290a0e6bb
aliyun_zhaown <activationzwn@126.com>
2014-10-20 16:53:12 +0800

    add pagination func

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@
    /logout:登出
  */
 
-var crypto = require('crypto'),User = require('../models/user.js'),Post = require('../models/post.js');
+var crypto = require('crypto'),User = require('../models/user.js'),Post = require('../models/post.js'),Comment=require('../models/comment.js');
 var fs = require('fs');
 //-----------------路由中间件----------------
   //类似过滤器
@@ -28,7 +28,8 @@ var fs = require('fs');
 //-------------------------------------------
 module.exports = function(app){
   app.get('/',function(req,res){
-         Post.getAll(null,function(err,posts){
+         var page = req.query.p?parseInt(req.query.p):1;
+         Post.getTen(null,page,function(err,posts,total){
              if(err){
                posts = [];
              }
@@ -36,6 +37,9 @@ module.exports = function(app){
                                title: 'little bird-主页'
                               ,user:req.session.user
                               ,posts:posts
+                              ,page:page
+                              ,isFirstPage:(page-1)==0
+                              ,isLastPage:((page-1)*10+posts.length)==total
                               ,success:req.flash('success').toString() 
                               ,error:req.flash('error').toString() 
          });
@@ -174,13 +178,13 @@ module.exports = function(app){
   });
 
   app.get("/u/:name",function(req,res){
-     
+     var page = req.query.p?parseInt(req.query.p):1;     
      User.get(req.params.name,function(err,user){
          if(!user){
            req.flash('error','用户不存在！');
            return res.redirect('/');
          }
-         Post.getAll(user.name,function(err,posts){
+         Post.getTen(user.name,page,function(err,posts,total){
              if(err){
                 req.flash('error',err);
                 return res.redirect('/');
@@ -188,6 +192,9 @@ module.exports = function(app){
              res.render('user',{
                 title:user.name,
                 posts:posts,
+                page:page,
+                isFirstPage:(page-1)==0,
+                isLastPage:((page-1)*10+posts.length)==total,
                 user:req.session.user,
                 success:req.flash('success').toString(),
                 error:req.flash('error').toString()
@@ -211,6 +218,28 @@ module.exports = function(app){
             error:req.flash('error').toString()
        });
     }); 
+  });
+
+  app.post('/u/:name/:day/:title',function(req,res){
+     var date = new Date(),time = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+date.getDate()+" "+date.getHours()+":"+(date.getMinutes()<10?'0'+date.getMinutes():date.getMinutes());
+     var comment = {
+          name:req.body.name,
+          email:req.body.email,
+          website:req.body.website,
+          time:time,
+          content:req.body.content
+     
+      };
+      var newComment = new Comment(req.params.name,req.params.day,req.params.title,comment);
+      newComment.save(function(err){
+          if(err){
+              req.flash('error',err);
+              return res.redirect('back');
+          }
+          req.flash('success','留言成功！');
+          res.redirect('back');
+      });
+ 
   });
 
   app.get('/edit/:name/:day/:title',checkLogin);
